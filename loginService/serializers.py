c0d21f02a73037e4e77ab6917afbf4656f9cb32b
munseok <moonseok1126@gmail.com>
2022-11-03 15:45:00 +0900

    loginservice 완성

---
+++ loginService/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'name', 'user_id', 'password', 'group', 'admin', 'created_at']
